06944480480f16cfcdfd39d04cbe509f0aa599f3
Augusto Lemos <augustolemosmza@gmail.com>
2026-01-08 18:59:24 -0300

    Primer commit: front y back juntos

---
+++ tesisHub/tesisHub/src/main/java/amsw/tesisHub/TesisHubApplication.java
@@ -0,0 +1,13 @@
+package amsw.tesisHub;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TesisHubApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TesisHubApplication.class, args);
+	}
+
+}
---
+++ tesisHub/tesisHub/src/test/java/amsw/tesisHub/TesisHubApplicationTests.java
@@ -0,0 +1,13 @@
+package amsw.tesisHub;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TesisHubApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
