5dfe64cf4572ff48789108dfbf5b2017cd49a613
macaepifani <macarenaepifani@gmail.com>
2026-01-17 08:11:17 -0300

    fase 0 front

---
+++ tesishubfront/src/pages/Home.jsx
@@ -0,0 +1,19 @@
+const Home = () => {
+  return (
+    <div>
+      <h1>TesisHub</h1>
+      <p>Repositorio de tesis universitarias</p>
+
+      <div>
+        <h3>Proyectos destacados</h3>
+        <ul>
+          <li>Proyecto 1</li>
+          <li>Proyecto 2</li>
+          <li>Proyecto 3</li>
+        </ul>
+      </div>
+    </div>
+  );
+};
+
+export default Home;
